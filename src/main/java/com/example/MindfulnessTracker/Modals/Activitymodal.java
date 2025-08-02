94ba2637fea2dd5583ed90e6f6be7fb1275bc39d
MONISHA U <monisha.u2023cse@sece.ac.in>
2025-08-02 14:43:11 +0530

    Initial commit

---
+++ src/main/java/com/example/MindfulnessTracker/Modals/Activitymodal.java
@@ -0,0 +1,28 @@
+package com.example.MindfulnessTracker.Modals;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity // optional, ensures table name
+public class Activitymodal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Ensures MySQL auto-generates ID
+    private Long activityid;
+
+    private String activityNames;
+
+    private String description;
+
+    // ensures date format stored properly
+    private Date dateofactivity;
+
+    private int durationMinutes;
+}
